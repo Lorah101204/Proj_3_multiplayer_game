37e0b412b38bdf1ea6fad88cc686fc04ccdbaa90
Huy Bùi <115094360+Lorah101204@users.noreply.github.com>
2025-10-27 23:30:53 +0700

    Import mirror, level data

--- Assets/Scripts/LevelDataSO.cs
+++ Assets/Scripts/LevelDataSO.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
-[CreateAssetMenu(fileName = "NewLevelData", menuName = "Level Data")]
+[CreateAssetMenu(fileName = "LevelDataSO", menuName = "Level Data")]
 public class LevelDataSO : ScriptableObject
 {
-    public string levelName;
+    public List<LevelData> levels;
+
+    public LevelData GetLevelData(int levelIndex)
+    {
+        if (levelIndex >= 0 && levelIndex < levels.Count)
+            return levels[levelIndex];
+        return null;
+    }
+
+    public int GetLevelByID(int id)
+    {
+        var leveldata = levels.FirstOrDefault(level => level.levelID == id);
+        return leveldata.level;
+    }
+
+    public string GetLevelByIndexD(int i)
+    {
+        var levelData = levels[i];
+        return levelData.levelName;
+    }
+
+    public int GetTotalLevels() => levels.Count;
+}
+
+[Serializable]
+public class LevelData
+{
     public int levelID;
-    public int maxPlayers;
+    public int level;
+    public string levelName;
+    public GameObject levelPrefab;
 }
